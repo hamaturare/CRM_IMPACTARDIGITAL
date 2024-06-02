3c5c6a316426bb0e0e10dd964f644ece38cfea61
hamaturare <felipemendes71@gmail.com>
2024-06-02 14:25:09 -0300

    adding admin to the wpmessage

--- apps/wpmessages/admin.py
+++ apps/wpmessages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import WpMessage
 
-# Register your models here.
+class WpMessageAdmin(admin.ModelAdmin):
+    list_display = ('lead_phone_number', 'profile_name', 'timestamp', 
+                    'business_phone_number', 'state', 'service_interest', 
+                    'contact_method', 'chat_history')
+    filter_display = ('lead_phone_number', 'profile_name','state', 'service_interest', 'contact_method')
+
+admin.site.register(WpMessage, WpMessageAdmin)
