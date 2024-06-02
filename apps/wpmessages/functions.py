3fa2bd919c1de39400a1619615ce763ced543cc4
hamaturare <felipemendes71@gmail.com>
2024-06-02 00:08:40 -0300

    testing if I can send a message when receivin one from a number

--- apps/wpmessages/functions.py
+++ apps/wpmessages/functions.py
@@ -3,19 +3,14 @@ import requests
 
 def send_whatsapp_message(phoneNumber, message):
     url = f"{settings.WHATSAPP_API_URL}/{settings.WHATSAPP_PHONE_ID}/messages"
-    headers = {
-        "Authorization": f"Bearer {settings.WHATSAPP_ACCESS_TOKEN}",
-        "Content-Type": "application/json"
-    }
-    payload = {
-        "messaging_product": "whatsapp",
-        "phone_number": phoneNumber,
-        "type": "text",
-        "text": {
-            "body": message
-        }
-    }
+    headers = {"Authorization": f"Bearer {settings.WHATSAPP_ACCESS_TOKEN}"}
+    payload = {"messaging_product": "whatsapp",
+                "recipent_type":"individual",
+                "to": phoneNumber,
+                "type": "text",
+                "text": {"body": message}
+                }
 
     response = requests.post(url, headers=headers, json=payload)
-    return response.json()
-
+    ans = response.json()
+    return ans
