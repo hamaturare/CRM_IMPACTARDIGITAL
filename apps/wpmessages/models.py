4c606bbeee88a2c850a51e51b6efe76f9290435a
hamaturare <felipemendes71@gmail.com>
2024-06-02 22:59:35 -0300

    Webhook Worked again updating models to accep blank values for testing. updating handling_message function to save into db some invo and reply back

--- apps/wpmessages/models.py
+++ apps/wpmessages/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 
 class WpMessage(models.Model):
+    lead_phone_number = models.CharField(max_length=15)  # Número de telefone da lead
+    profile_name = models.CharField(max_length=100, blank=True, null=True)  # Nome do perfil da lead
+    whatsapp_id = models.CharField(max_length=20, blank=True, null=True)  # ID do WhatsApp da lead
+    message_id = models.CharField(max_length=50, blank=True, null=True)  # ID da mensagem
+    timestamp = models.DateTimeField(blank=True, null=True)  # Timestamp da mensagem
+    message_text = models.TextField()  # Texto da mensagem
+    business_phone_number = models.CharField(max_length=15, blank=True, null=True)  # Número de telefone do negócio (Our number) que contactou a lead, que envia a mensagem
+    phone_id = models.CharField(max_length=20, blank=True, null=True)  # ID do telefone do negócio (Our number)
+    chat_history = models.TextField(blank=True, null=True)  # Histórico de chat
+
+    state = models.CharField(max_length=50, default='initial', blank=True, null=True)  # Status do chat
+    thanked = models.BooleanField(default=False)  # Chat finalizado com obrigado
+    service_interest = models.CharField(max_length=100, blank=True, null=True)  # Interesse no serviço
+    contact_method = models.CharField(max_length=50, blank=True, null=True)  # Método de contato preferido
+
+    def __str__(self):
+        return self.lead_phone_number
+
+
+"""
+class WpMessage(models.Model):
     lead_phone_number = models.CharField(max_length=15) # Número de telefone da lead
     profile_name = models.CharField(max_length=100) # Nome do perfil da lead
     whatsapp_id = models.CharField(max_length=20) # ID do WhatsApp da lead
@@ -18,3 +39,4 @@ class WpMessage(models.Model):
 
     def __str__(self):
         return self.lead_phone_number
+"""
