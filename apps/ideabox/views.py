7e838dfe653cbcfcd89e0473c8cecfefaaa03ef5
hamaturare <felipemendes71@gmail.com>
2024-06-16 18:41:36 -0300

    Adding send email to UpdateView, making the code DRY

--- apps/ideabox/views.py
+++ apps/ideabox/views.py
@@ -17,58 +17,18 @@ import threading
 # Set up logging
 logger = logging.getLogger(__name__)
 
-class SubmitSuggestionView(LoginRequiredMixin, CreateView):
-    model = Suggestion
-    form_class = SuggestionForm
-    template_name = 'ideabox/submit_suggestion.html'
-    success_url = reverse_lazy('suggestions_list')
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['user'] = self.request.user
-        return kwargs
-    
-    def post(self, request, *args, **kwargs):
-        # Adiciona a mensagem antes de processar o formulário
-        messages.success(self.request, 'Sugestão Enviada Com Sucesso')
-        return super().post(request, *args, **kwargs)
-    
-    def form_valid(self, form):
-        # Associa o usuário logado à instância do formulário antes de salvar
-        form.instance.user = self.request.user
-        response = super().form_valid(form)  # Isso salva o objeto
-
-        # Get the admin emails from settings.EMAIL_ADMINS
-        admin_emails = settings.EMAIL_ADMINS
-
-        # Get the user's email
-        user_email = self.request.user.email
-
-        # Combine admin emails and the user's email into a flat list
-        recipient_list = admin_emails + [user_email]
-
-        # Send email in a new thread to avoid blocking the response
-        thread = threading.Thread(target=self.send_email, args=(recipient_list, form.instance))
-        thread.start()
-
-        return response
-
-    def send_email(self, recipient_list, suggestion):
-        try:
-            send_mail(
-                'Nova Sugestão Recebida',  # Assunto do email
-                'Uma nova sugestão foi submetida por {}. \n\nTítulo: {}\n\nSugestão: {}'.format(
-                    self.request.user.username,
-                    suggestion.title,
-                    suggestion.content
-                ),  # Mensagem
-                settings.EMAIL_HOST_USER,  # Email do remetente
-                recipient_list,  # Lista de emails que receberão a mensagem
-                fail_silently=False,  # Se True, suprime as exceções de SMTP
-            )
-        except Exception as e:
-            # Log the error with more details
-            logger.error(f"Failed to send email to {recipient_list}: {e}")
+def send_email_async(recipient_list, subject, message):
+    try:
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            recipient_list,
+            fail_silently=False,
+        )
+        logger.info(f"Email sent to {recipient_list}")
+    except Exception as e:
+        logger.error(f"Failed to send email to {recipient_list}: {e}")
 
 class SuggestionsListView(LoginRequiredMixin, ListView):
     model = Suggestion
@@ -94,6 +54,38 @@ class SuggestionsListView(LoginRequiredMixin, ListView):
 
         return queryset
 
+class SubmitSuggestionView(LoginRequiredMixin, CreateView):
+    model = Suggestion
+    form_class = SuggestionForm
+    template_name = 'ideabox/submit_suggestion.html'
+    success_url = reverse_lazy('suggestions_list')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+    
+    def post(self, request, *args, **kwargs):
+        messages.success(self.request, 'Sugestão Enviada Com Sucesso')
+        return super().post(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        response = super().form_valid(form)
+
+        admin_emails = settings.EMAIL_ADMINS
+        user_email = self.request.user.email
+        recipient_list = admin_emails + [user_email]
+        subject = 'Nova Sugestão Recebida'
+        message = 'Uma nova sugestão foi submetida por {}. \n\nTítulo: {}\n\nSugestão: {}\n\nImplementaremos o mais Rápido possível\n\n Att,\n\n Admin 😎'.format(
+            self.request.user.username,
+            form.instance.title,
+            form.instance.content
+        )
+
+        threading.Thread(target=send_email_async, args=(recipient_list, subject, message)).start()
+        return response
+
 class SuggestionUpdateView(LoginRequiredMixin, UpdateView):
     model = Suggestion
     form_class = SuggestionUpdateForm
@@ -101,5 +93,15 @@ class SuggestionUpdateView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('suggestions_list')
 
     def form_valid(self, form):
-        messages.success(self.request, 'Sugestão atualizada com sucesso!')
-        return super().form_valid(form)
+        original_status = self.get_object().status
+        response = super().form_valid(form)
+
+        if form.instance.status != original_status:
+            admin_emails = settings.EMAIL_ADMINS
+            user_email = self.request.user.email
+            recipient_list = admin_emails + [user_email]
+            subject = 'Sugestão Atualizada'
+            message = f'Status da sugestão "{form.instance.title}" foi atualizado para {form.instance.status}.\n\n Att,\n\n Admin 😎'
+            
+            threading.Thread(target=send_email_async, args=(recipient_list, subject, message)).start()
+        return response
