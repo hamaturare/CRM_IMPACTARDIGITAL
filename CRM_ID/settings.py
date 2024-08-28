985f512956a249ca213c3192ff8853f30ff65474
hamaturare <felipemendes71@gmail.com>
2024-08-28 15:08:38 -0300

    changing settings.pu

--- CRM_ID/settings.py
+++ CRM_ID/settings.py
@@ -199,9 +199,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media Files
