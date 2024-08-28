653774dc1638ccaf5761171e333b7ce72d49a59c
hamaturare <felipemendes71@gmail.com>
2024-08-28 15:04:29 -0300

    Adding crmid.conf and uwsgi_params and static_root env conf

--- CRM_ID/settings.py
+++ CRM_ID/settings.py
@@ -202,11 +202,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media Files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
